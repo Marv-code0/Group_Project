1d66e705e4d17228a42ddf24442f6b7ebd418f38
Marvin Grassl <marvin.grassl@student.tugraz.at>
2026-05-31 14:00:16 +0200

    first start on environment.py

--- src/environment.py
+++ src/environment.py
@@ -2,9 +2,78 @@
 Environment setup for the mass panic simulation.
 
 The environment represents a simplified stadium / concert area:
-- The grid is the stadium floor / field.
+- The grid is the stadium floor / field. rectangular
 - People start inside the field area.
 - Exits are specific cells at the grid border.
 - Obstacles represent fences, barriers, stage areas or blocked zones.
 """
+
+from dataclasses import dataclass
+from enum import IntEnum
+import numpy as np
+
+
+EXIT_NUM = 4
+
+class CellType(IntEnum):
+    FREE = 0
+    STAGE = 1
+    OBSTACLE = 2
+    EXIT = 3
+
+@dataclass
+class EnvironmentConfig:
+
+    #variable
+    exit_width: int
+    use_stage: bool # if the stage in front is a blocked field
+    use_barriers: bool # if there are f.e fences that an agent need to go around
+
+    #crowd control
+    max_agents_per_cell: int
+
+    border_spawn_protection: int #how far from the borders an agent should spawn
+
+    # fixed
+    width: int = 100
+    height: int = 80
+
+#creates an environment grid for the model with the exits and the obsticales/stage grid[x,y] = 1 means this grid is type stage
+def create_environment_grid(config: EnvironmentConfig) -> np.ndarray:
+    pass
+
+def add_exits(grid: np.ndarray, config: EnvironmentConfig) -> None:
+    """
+    add the exits to the grid
+    2 on the left side
+    2 on the right side
+    2 at the bottom side
+    """
+    # Left/right side: split height into gap | exit | gap | exit | gap
+
+    gap_y = (config.height - 2 * config.exit_width) // 3
+    first_exit_y_start = gap_y
+    second_exit_y_start = 2*gap_y + config.exit_width
+
+    for y in range(first_exit_y_start, first_exit_y_start + config.exit_width):
+        grid[0, y] = CellType.EXIT
+        grid[config.width - 1, y] = CellType.EXIT
+
+    for y in range(second_exit_y_start, second_exit_y_start + config.exit_width):
+        grid[0, y] = CellType.EXIT
+        grid[config.width - 1, y] = CellType.EXIT
+
+    # Bottom side: split width into gap | exit | gap | exit | gap
+    gap_x = (config.width - 2 * config.exit_width) // 3
+
+    first_exit_x_start = gap_x
+    second_exit_x_start = 2 * gap_x + config.exit_width
+
+    for x in range(first_exit_x_start, first_exit_x_start + config.exit_width):
+        grid[x, 0] = CellType.EXIT
+
+    for x in range(second_exit_x_start, second_exit_x_start + config.exit_width):
+        grid[x, 0] = CellType.EXIT
+
+
 
--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
 print("hello")
 # Main to start simulation
+# We want to simulate a masspanic in an arena
+# Due to the limited computing power we simulate with a few agents f.e. 4000 but count one agent as f.e. 20 persons.
+from environment import EnvironmentConfig
+
+def main():
+    real_people = 40000
+    people_per_agent = 20
+
+    num_agents = real_people//people_per_agent
+
+    env_config = EnvironmentConfig(
+            exit_width=8,
+            use_stage=True,
+            use_barriers=True,
+
+            max_agents_per_cell = 4,
+            border_spawn_protection = 2,
+    )
+
